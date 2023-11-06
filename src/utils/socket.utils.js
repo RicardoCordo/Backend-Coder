7eb1abc3687070a084b15137842b81720ee19e24
ricardo cordo <ricardocordo93@gmail.com>
2023-11-06 14:11:48 -0300

    correcciones

--- src/utils/socket.utils.js
+++ src/utils/socket.utils.js
@@ -1,34 +1,42 @@
-import { messageModel } from "../dao/mongo/models/messages.model.js"
+import { messageModel } from "../dao/mongo/models/messages.model.js";
 import { Server } from "socket.io";
-import products from "../data/products.json" assert { type: "json" };
 import logger from "./logger.utils.js";
+import getProductsController from "../controllers/products.controller.js"
 
-function socketSetup (httpServer){
-const io = new Server(httpServer);
-	const messages = [];
-	io.on("connection", (socket) => {
-		logger.info("Nuevo cliente conectado");
+async function socketSetup(httpServer) {
+  const io = new Server(httpServer);
+  const messages = [];
+
+  io.on("connection", async (socket) => {
+    logger.info("Nuevo cliente conectado");
+
+	try {
+		const products = await getProductsController();
 		socket.emit("products", products);
-		io.emit("messageLogs", messages);
-		socket.on("user", data => {
-			messages.push(data);
-			io.emit("messagesLogs", messages);
-		});
-	socket.on("message", data => {
-		messages.push(data);
-		io.emit("messagesLogs", messages);
-		messageModel.create({
-			user: data.user,
-			message: data.message,
-		});
-	});
+	  } catch (error) {
+		logger.error("Error al obtener productos:", error);
+	  }
 
+    io.emit("messageLogs", messages);
 
-	socket.on("disconnect", () => {
-		logger.info("Cliente desconectado");
-	});
+    socket.on("user", (data) => {
+      messages.push(data);
+      io.emit("messagesLogs", messages);
+    });
 
-});
+    socket.on("message", (data) => {
+      messages.push(data);
+      io.emit("messagesLogs", messages);
+      messageModel.create({
+        user: data.user,
+        message: data.message,
+      });
+    });
+
+    socket.on("disconnect", () => {
+      logger.info("Cliente desconectado");
+    });
+  });
 }
 
-export default socketSetup
+export default socketSetup;
